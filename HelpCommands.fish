cefb6ec26c5a938cda6c80ea303bf7b67069fb5c
Ahmed Al Busaidi <ahmedbusaidi82@gmail.com>
2022-10-04 12:59:57 -0400

    Better Error Handling & Better Clean Up

--- HelpCommands.fish
+++ HelpCommands.fish
@@ -1,15 +1,19 @@
 #!/usr/bin/env fish
-
-function GetWorkingDir -a dir -d "Get the current working directory"
+function OpenVSCodeIn -a dir -d "Get the working directory"
     if string match -q "personal" $dir
-        cd ~/work/personal
+        cd ~/personal
+        clear
         code .
     end
 
     if string match -q "work" $dir
         cd ~/work
+        clear
         code .
+
+    else 
+        echo "🔴 Operation Not Permitted Please Select Either [personal] or [work] 🔴"
     end
+end
 
-
-funcsave GetWorkingDir
+funcsave OpenVSCodeIn
